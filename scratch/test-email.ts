0bdf3eff33a1dd311a227bf48b04f1dd98c4a951
Intern-Yago <yagovictorbotafogo@gmail.com>
2026-08-16 23:10:39 -0300

    chore: remove unused scratch directory

--- scratch/test-email.ts
+++
@@ -1,9 +0,0 @@
-import { sendTestEmail } from "../src/services/email-service.js";
-
-async function main() {
-  console.log("Enviando e-mail de teste para yago.commercial@gmail.com via Gmail SMTP...");
-  const res = await sendTestEmail("yago.commercial@gmail.com");
-  console.log("Resultado do envio:", res);
-}
-
-main().catch(console.error);
